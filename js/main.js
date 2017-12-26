27cc946b4072c25b30b939b257c687ed85f5678a
Sergey Jar <serejajar@yandex.ru>
2017-12-26 14:01:13 +0200

    added swipe event

--- js/main.js
+++ js/main.js
@@ -5,9 +5,17 @@ var dom = {
 var hammertime = new Hammer(dom.wrapper, {direction: Hammer.DIRECTION_ALL});
 
 hammertime.on('swipeleft swiperight', function(ev) {
-    dom.status.innerHTML = ev.type;
-    var wrapperPosition = parceInt(dom.wrapper.style.left);
+    var wrapperPosition = parseInt(dom.wrapper.style.left) || 0;
+    var wrapperWidth = dom.wrapper.offsetWidth;
+    var parentWidth = dom.wrapper.parentElement.offsetWidth;
 
-    var goTo = ev.type === 'swipeleft' ? wrapperPosition - 1000 : wrapperPosition + 1000;
-    dom.wrapper.style.left = wrapperPosition + '';
+    var goTo = ev.type === 'swipeleft' ? (wrapperPosition - 500) : (wrapperPosition + 500);
+    if (goTo > 0) {
+        goTo = 0;
+    }
+    if (Math.abs(goTo) + parentWidth > wrapperWidth) {
+        goTo = parentWidth - wrapperWidth;
+    }
+    dom.wrapper.style.left = `${goTo}px`;
+    dom.status.innerHTML = ev.type;
 });
